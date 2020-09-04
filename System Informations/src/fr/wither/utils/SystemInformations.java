b40894fe82f750b90f60404f6608501aac712841
__Wither <wither.plugins@gmail.com>
2020-09-04 16:27:15 +0200

    Added javaDoc commentaries

--- System Informations/src/fr/wither/utils/SystemInformations.java
+++ System Informations/src/fr/wither/utils/SystemInformations.java
@@ -5,36 +5,73 @@ import java.lang.management.ManagementFactory;
 
 import com.sun.management.OperatingSystemMXBean;
 
-@SuppressWarnings("restriction")
+/**
+ * 
+ * La seule et unique classe de SystemInformations, elle contient les méthodes pour obtenir les information système.
+ * @author Wither
+ * @version 0.1.0
+ * 
+ */
+
 public class SystemInformations {
 	
-	//Choix du facteur entre un octet et un Go (Gio pour etre exact dans ce cas-ci)
+
+	public SystemInformations() {
+		
+	}
+	
+	/**
+	 * Choix du facteur entre un octet et un Go (Gio pour etre exact dans ce cas-ci)
+	 */
 	public static final long BYTES_TO_GO = 1073741824 ;
 	
-	//retourne le maximum de mémoire vive(RAM) de l'ordinateur. /!\ peut être sous-estimée
-	public long getSystemRAM() {
+	/**
+	 * Méthode d'obtention de la RAM totale de l'ordinateur
+	 * @since 0.0.1
+	 * @return
+	 * le maximum de mémoire vive(RAM) de l'ordinateur. /!\ peut être sous-estimée
+	 */
+	public static final long getSystemRAM() {
 		return ((OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean()).getTotalPhysicalMemorySize() / BYTES_TO_GO;
 	}
 	
-	//retourne Unknown, 32 ou 64 suivant l'architecture java (32 bits, 64 bits, Inconnue/Indeterminable)
-	public String getJavaBits() {
+	/**
+	 * Méthode d'obtention du type d'architecture java
+	 * @since 0.0.1
+	 * @return
+	 * 	Unknown, 32 ou 64 suivant l'architecture java (32 bits, 64 bits, Inconnue/Indeterminable)
+	 */
+	public static final String getJavaBits() {
 		return System.getProperty("sun.arch.data.model");
 	}
 	
-	//retourne l'espace disque total
-	public long getDiskSize() {
+	/**
+	 * Méthode d'obtention de l'espace disque
+	 * @since 0.0.1
+	 * @return
+	 * l'espace disque total
+	 */
+	public static final long getDiskSize() {
 		return new File("/").getTotalSpace() / BYTES_TO_GO;
 	}
 	
-	
-	//Retourne la version java sous la forme X.X
-	public String getJavaVersion() {
+	/**
+	 * La première méthode pour la version de java
+	 * @since 0.0.1
+	 * @return
+	 * la version java sous la forme X.X
+	 */
+	public static final String getJavaVersion() {
 		return System.getProperty("java.specification.version");
 	}
 	
-	
-	//Retourne la version de java sous la forme X.X.X_XXX
-	public String getExactJavaVersion() {
+	/**
+	 * La seconde méthode pour la version de java
+	 * @since 0.0.1
+	 * @return
+	 * la version de java sous la forme X.X.X_XXX
+	 */
+	public static final String getExactJavaVersion() {
 		return System.getProperty("java.version");
 	}
 }
